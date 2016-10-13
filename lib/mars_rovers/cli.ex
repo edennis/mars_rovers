43291bc2496abb218e5761bef28e605dead9c4f5
Erick Dennis <erick.dennis@gmail.com>
2016-10-13 12:22:07 +0200

    Use OptionParser in CLI
    * Add help/version options

--- lib/mars_rovers/cli.ex
+++ lib/mars_rovers/cli.ex
@@ -1,15 +1,35 @@
 alias MarsRovers.InputParser
 
 defmodule MarsRovers.CLI do
+  @bin_name :mars_rovers
+
   def main(args) do
     args |> parse_args |> process
   end
 
-  defp parse_args([]), do: [:help]
-  defp parse_args([file | _]), do: [file: file]
+  defp parse_args(args) do
+    parse =
+      with switches = [help: :boolean, version: :boolean],
+           aliases  = [h: :help, v: :version],
+      do:
+           OptionParser.parse(args, switches: switches, aliases: aliases)
 
-  def process([:help]) do
-    IO.puts "Usage: ./mars_rovers <input_file>"
+    case parse do
+      {[{switch, true}], _, _} -> switch
+      {_, [filename], _}       -> [file: filename]
+      default                  -> :help
+    end
+  end
+
+  def process(:help) do
+    IO.write """
+    Usage: #{@bin_name} [-h | --help] [-v | --version] <input_file>
+    """
+  end
+
+  def process(:version) do
+    {:ok, version} = :application.get_key(:mars_rovers, :vsn)
+    IO.puts "#{@bin_name} version #{version}"
   end
 
   def process(file: file) do
